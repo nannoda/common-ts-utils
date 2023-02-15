6d39e48de3c8c781999c6477a6c7f333c6420db2
NaN Noda <nananannoda@gmail.com>
2023-02-14 21:27:00 -0600

    Update: some common typescript codes

---
+++ Math/Vector.ts
@@ -0,0 +1,8 @@
+type Vec2 = [number, number];
+type Vec3 = [number, number, number];
+type Vec4 = [number, number, number, number];
+
+type Mat2 = [Vec2, Vec2];
+type Mat3 = [Vec3, Vec3, Vec3];
+type Mat4 = [Vec4, Vec4, Vec4, Vec4];
+
